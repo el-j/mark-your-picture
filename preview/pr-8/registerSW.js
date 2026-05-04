a1fbaeed581619a0d4ca1ba3dcac6c08c11696b3
el-j <el-j@users.noreply.github.com>
2026-05-04 15:56:53 +0000

    deploy: fc9658d7139dbbf981659700f02efc3d693cdda8

---
+++ preview/pr-8/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/mark-your-picture/preview/pr-8/sw.js', { scope: '/mark-your-picture/preview/pr-8/' })})}
